17bacd21b41b2ecf5566fab2627ee86aaace433a
cy <1679306431@qq.com>
2020-03-01 14:20:59 +0800

    基本结构

--- src/component/Suggest.js
+++ src/component/Suggest.js
@@ -2,13 +2,32 @@ import React from 'react';
 import jsonp from 'jsonp';
 
 class Suggest extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            words:['1','2','3']
+        }
+    }
+
+    handleChange = (event)=>{
+        let code = event.target.keycode;
+    }
+
     render(){
         return <div className="container">
             <div className="row">
                 <div className="col-sm-8 col-sm-offset-2">
                     <div className="panel panel-default">
-                        <div className="panel-heading">Tou</div>
-                        <div className="panel-body"></div>
+                        <div className="panel-heading">
+                            <input className="form-control" onChange={this.handleChange}></input>
+                        </div>
+                        <div className="panel-body">
+                            <ul className="list-group">
+                            {this.state.words.map((item,index)=>{
+                                return <li key={index} className="list-group-item">{item}</li>
+                            })}
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/index.js
+++ src/index.js
@@ -3,14 +3,5 @@ import ReactDOM from 'react-dom';
 import Suggest from './component/Suggest.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-class Panel extends React.Component{
-    render(){
-        return <div className="panel panel-default">
-            <div className="panel-heading">tou</div>
-            <div className="panel-body">ti</div>
-            <div className="panel-footer">wei</div>
-        </div>
-    }
-}
-ReactDOM.render(<Panel/>, document.getElementById('root'));
+ReactDOM.render(<Suggest/>, document.getElementById('root'));
 
