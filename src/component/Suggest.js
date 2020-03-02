c834507e6194aab413d8b63a253dbd9927aad18d
cy <1679306431@qq.com>
2020-03-02 21:23:16 +0800

    百度搜索框完整版

--- src/component/Suggest.js
+++ src/component/Suggest.js
@@ -5,12 +5,40 @@ class Suggest extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            words:['1','2','3']
+            words:['1','2','3'],
+            index :-1
         }
     }
 
     handleChange = (event)=>{
-        let code = event.target.keycode;
+        let wd = event.target.value;
+        this.wd = wd;
+        jsonp(`http://www.baidu.com/su?wd=${wd}`,{param:"cb"},(err,data)=>{
+            console.log(data);
+            this.setState({
+                words:data.s
+            })
+        });
+    }
+
+    handleKeyDown = (event)=>{
+        let code = event.keyCode;
+        let index = this.state.index;
+        if(code === 38){
+            index --;
+            if (index === -2){
+                index = this.state.words.length-1;
+            }
+        } else if (code === 40){
+            index ++;
+            if(index === this.state.words.length){
+                index = -1;
+            } 
+        } else if (code === 13) {
+            console.log(event.target.value);
+            window.location = `http://www.baidu.com/s?wd=${event.target.value}`;
+        }
+        this.setState({index});
     }
 
     render(){
@@ -19,12 +47,12 @@ class Suggest extends React.Component{
                 <div className="col-sm-8 col-sm-offset-2">
                     <div className="panel panel-default">
                         <div className="panel-heading">
-                            <input className="form-control" onChange={this.handleChange}></input>
+                            <input className="form-control" value={(this.state.index === -1? this.wd:this.state.words[this.state.index])} onChange={this.handleChange} onKeyDown={this.handleKeyDown}></input>
                         </div>
                         <div className="panel-body">
                             <ul className="list-group">
                             {this.state.words.map((item,index)=>{
-                                return <li key={index} className="list-group-item">{item}</li>
+                                return <li key={index} className={"list-group-item "+(this.state.index===index?"active":"")}>{item}</li>
                             })}
                             </ul>
                         </div>
